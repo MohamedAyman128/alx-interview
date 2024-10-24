d70159be779d46bf235bc52abe077370eacda2ea
Mohamed Ayman <mmohamedaayman1315125@gmail.com>
2024-10-24 19:58:01 +0300

    0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,67 @@
 #!/usr/bin/python3
 
+<<<<<<< HEAD
+'''
+Method to print
+
+'''
+
+=======
+"""
+method to print
+"""
+>>>>>>> eafcb17d1cafd5a13a68f70614b23ef9958c322e
 import sys
 
 
-def print_msg(dict_sc, total_file_size):
+def printer(dict_sc, TFile_size):
     """
     Method to print
     Args:
         dict_sc: dict of status codes
-        total_file_size: total of the file
+        TFile_size: total of the file
     Returns:
-        Nothing
+        None
     """
 
-    print("File size: {}".format(total_file_size))
-    for key, val in dict_sc.items():
+    print("File size: {}".format(TFile_size))
+    for key, val in sorted(dict_sc.items()):
         if val != 0:
-            print("{:s}: {:d}".format(key, val))
+            print("{}: {}".format(key, val))
 
 
-total_file_size = 0
+TFile_size = 0
 code = 0
-counter = 0
+c = 0
 dict_sc = {
-    "200": 0, "301": 0,
-    "400": 0, "401": 0,
-    "403": 0, "404": 0,
-    "405": 0, "500": 0
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
 }
 
 try:
     for line in sys.stdin:
         parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
         if len(parsed_line) > 2:
-            counter += 1
+            c += 1
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[-1])
-                code = parsed_line[-2]
+            if c <= 10:
+                TFile_size += int(parsed_line[0])
+                code = parsed_line[1]
 
-                if (code in dict_sc.keys()):
+                if code in dict_sc.keys():
                     dict_sc[code] += 1
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
+            if c == 10:
+                printer(dict_sc, TFile_size)
+                c = 0
 
 finally:
-    print_msg(dict_sc, total_file_size)
+    printer(dict_sc, TFile_size)
