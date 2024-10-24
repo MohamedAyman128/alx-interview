24c9638024b753792870c01a129b82b935d7a2a1
Mohamed Ayman <mmohamedaayman1315125@gmail.com>
2024-10-24 19:27:15 +0300

    0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,34 +14,31 @@ def print_msg(dict_sc, total_file_size):
     """
 
     print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
+    for key, val in dict_sc.items():
         if val != 0:
-            print("{}: {}".format(key, val))
+            print("{:s}: {:d}".format(key, val))
 
 
 total_file_size = 0
 code = 0
 counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+dict_sc = {
+    "200": 0, "301": 0,
+    "400": 0, "401": 0,
+    "403": 0, "404": 0,
+    "405": 0, "500": 0
+}
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+        parsed_line = line.split()
 
         if len(parsed_line) > 2:
             counter += 1
 
             if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
+                total_file_size += int(parsed_line[-1])
+                code = parsed_line[-2]
 
                 if (code in dict_sc.keys()):
                     dict_sc[code] += 1
