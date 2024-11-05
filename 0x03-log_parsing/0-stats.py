aa27accba41febe521647ab8adad0dc4270e2af6
Mohamed Ayman <mmohamedaayman1315125@gmail.com>
2024-11-06 00:57:54 +0200

    0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,54 @@
 #!/usr/bin/python3
-
+"log parsing"
+import signal
 import sys
+import re
 
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in dict_sc.items():
-        if val != 0:
-            print("{:s}: {:d}".format(key, val))
+count = 0
+file_size = 0
+status_count = {c: 0 for c in ['200', '301', '400', '401',
+                               '403', '404', '405', '500']}
+pattern = (
+    r'(?P<ip>(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}'
+    r'(?:25[0-5]|2[0-4]\d|[01]?\d\d?))'
+    r' - \[(?P<date>.+)\] "GET /projects/260 HTTP/1.1"'
+    r' (?P<status>200|301|400|401|403|404|405|500) (?P<size>\d+)'
+)
 
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {
-    "200": 0, "301": 0,
-    "400": 0, "401": 0,
-    "403": 0, "404": 0,
-    "405": 0, "500": 0
-}
+def report():
+    "print a summary of the logs"
+    global file_size
+    global status_count
+    print(f'File size: {file_size}')
+    for code, count in status_count.items():
+        if count == 0:
+            continue
+        print(f'{code}: {count}')
 
-try:
+
+def handler(signum, frame):
+    "SIGINT handler"
+    report()
+    raise KeyboardInterrupt
+
+
+signal.signal(signal.SIGINT, handler)
+
+
+if __name__ == "__main__":
+    pattern = re.compile(pattern)
+
     for line in sys.stdin:
-        parsed_line = line.split()
-
-        if len(parsed_line) > 2:
-            counter += 1
-
-            if counter <= 10:
-                total_file_size += int(parsed_line[-1])
-                code = parsed_line[-2]
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+        match = pattern.fullmatch(line.strip())
+        if not match:
+            continue
+        count += 1
+        file_size += int(match.groupdict()['size'])
+        status_count[match.groupdict()['status']] += 1
+        if count == 10:
+            report()
+            count = 0
+    if 10 - count > 0:
+        report()
