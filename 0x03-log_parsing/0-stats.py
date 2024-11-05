a4a58038ad4f6c0f670306bc80ad1c6c0d82e5ec
Mohamed Ayman <mmohamedaayman1315125@gmail.com>
2024-11-06 00:59:12 +0200

    0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,57 @@
 #!/usr/bin/python3
-"log parsing"
-import signal
+"""
+Read stdin line by line and computes metrics
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>, skip line if not this format
+After every 10minutes or keyboard interrupt (CTRL + C)
+print these from beginning: number of lines by status code
+possible status codes: 200, 301, 400, 401, 404, 405, and 500
+if status code isn't an integer, do not print it
+format: <status code>: <number>
+Status code must be printed in ascending order
+"""
 import sys
-import re
 
 
-count = 0
+def print_msg(codes, file_size):
+    print("File size: {}".format(file_size))
+    for key, val in sorted(codes.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
 file_size = 0
-status_count = {c: 0 for c in ['200', '301', '400', '401',
-                               '403', '404', '405', '500']}
-pattern = (
-    r'(?P<ip>(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}'
-    r'(?:25[0-5]|2[0-4]\d|[01]?\d\d?))'
-    r' - \[(?P<date>.+)\] "GET /projects/260 HTTP/1.1"'
-    r' (?P<status>200|301|400|401|403|404|405|500) (?P<size>\d+)'
-)
+code = 0
+count_lines = 0
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-
-def report():
-    "print a summary of the logs"
-    global file_size
-    global status_count
-    print(f'File size: {file_size}')
-    for code, count in status_count.items():
-        if count == 0:
-            continue
-        print(f'{code}: {count}')
-
-
-def handler(signum, frame):
-    "SIGINT handler"
-    report()
-    raise KeyboardInterrupt
-
-
-signal.signal(signal.SIGINT, handler)
-
-
-if __name__ == "__main__":
-    pattern = re.compile(pattern)
-
+try:
     for line in sys.stdin:
-        match = pattern.fullmatch(line.strip())
-        if not match:
-            continue
-        count += 1
-        file_size += int(match.groupdict()['size'])
-        status_count[match.groupdict()['status']] += 1
-        if count == 10:
-            report()
-            count = 0
-    if 10 - count > 0:
-        report()
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
+
+        if len(parsed_line) > 2:
+            count_lines += 1
+
+            if count_lines <= 10:
+                file_size += int(parsed_line[0])
+                code = parsed_line[1]
+
+                if (code in codes.keys()):
+                    codes[code] += 1
+
+            if (count_lines == 10):
+                print_msg(codes, file_size)
+                count_lines = 0
+
+finally:
+    print_msg(codes, file_size)
